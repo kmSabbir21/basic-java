a89d94874dbe07877ac7efb4cf683b0eb76c5d08
Sabbir <kmsabbirkhan225@gmail.com>
2020-10-09 23:07:38 -0700

    git update

--- BasicJava/src/Biodata.java
+++ BasicJava/src/Biodata.java
@@ -2,11 +2,13 @@
 public class Biodata {
     public static void main(String [] args){
       System.out.println("\"1\t2\t3\"");
-      System.out.println("3\t2\t1");
       System.out.println("2\t3\t1");
         
         
         //System.out.println("Sabbir \n01744266394\nBongram");
+       //  System.out.println("Bongram");
+
+ 	//System.out.println("Sabbir \n01744266394\nBongram");
        // System.out.println("01744266394");
        //  System.out.println("Bongram");
         
