f585d65dc811feae7633b4af1e7f1c88f80b88ee
kmsabbir <kmsabbirkhan225@gmail.com>
2020-11-03 00:16:44 +0600

    Clear concept of oop

---
+++ Housold-oop/Brand.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package household;
+
+/**
+ *
+ * @author HP
+ */
+public class Brand {
+    private double brandId;
+
+    public double getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(double brandId) {
+        this.brandId = brandId;
+    }
+}
---
+++ Housold-oop/Worker.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package household;
+
+/**
+ *
+ * @author HP
+ */
+public class Worker {
+    private Door door;
+    private Window window;
+    private Brand brand;
+
+    public Door getDoor() {
+        return door;
+    }
+
+    public void setDoor(Door door) {
+        this.door = door;
+    }
+
+    public Window getWindow() {
+        return window;
+    }
+
+    public void setWindow(Window window) {
+        this.window = window;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+    
+}
