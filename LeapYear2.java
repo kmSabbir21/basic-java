94462cfe6325c5694e7c4ff862bd85c774d2ed23
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-04 00:17:32 +0600

    Update LeapYear2.java

--- LeapYear2.java
+++ LeapYear2.java
@@ -1,16 +1,13 @@
-
 package homework;
 
-public class LeapYear2 {
+public class LeapYear{
     public static void main(String[] args){
         int year=3030;
-                
         if((year%4==0)||(year%400==0 && year%100!=0)){
-        
         System.out.println("leap year");
         }
         else{
-        System.out.println("no its not");
+        System.out.println("No!It's not Leap Year");
         }
        
     }
