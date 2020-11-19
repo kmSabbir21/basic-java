9d6728829b85c253ac89f02279d44a87c971bb74
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-20 02:52:45 +0600

    Add files via upload

---
+++ ThreeDAnnomiousArray.java
@@ -0,0 +1,28 @@
+package New.folder;
+
+public class ThreeDAnnomiousArray {
+     static void sum(int [][][] no){
+        int total=0;
+        
+        for(int iii[][]:no){
+            
+           for(int ii[]:iii){
+               
+             for(int i:ii){
+                 
+              total=total+i;
+                }
+            
+            }
+           
+        }
+         System.out.println(total);
+    }
+    public static void main(String[] args) {
+        
+        ThreeDAnnomiousArray.sum(new int [][][]{{{1,2,3},{3,4,5}},{{20}}});
+        
+    }
+           
+}
+
