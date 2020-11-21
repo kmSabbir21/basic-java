431749682be205c62eb66f616001815eea659626
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-22 00:43:31 +0600

    Add files via upload

---
+++ SecondSmalestestEOfArray.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package New.folder;
+
+/**
+ *
+ * @author WIN
+ */
+public class SecondSmalestestEOfArray {
+     public static void main(String[] args) {
+        int [] a = {2,3,6,9,1,8,7,4};
+        for(int i=0;i<a.length;i++){
+            
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]>a[j]){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                }
+        
+            }
+       }
+        System.out.println(a[1]);
+    }
+    
+    
+}
