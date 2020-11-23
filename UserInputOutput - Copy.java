8b5ea0e576c33e9ebed0cba3f3f8b14acc939bba
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-23 22:17:24 +0600

    Update UserInputOutput - Copy.java

--- UserInputOutput - Copy.java
+++ UserInputOutput - Copy.java
@@ -2,7 +2,7 @@ package diunotes;
 import java.util.Scanner;
 public class UserInputOutput {
     
-   public static void main(String[] args) {
+  public static void main(String[] args) {
         
         int x,a;
         double y,b;
