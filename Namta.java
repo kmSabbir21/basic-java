4c6ab0d83819a177d5e31ad5806b1b0b7bf00ba6
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-12-02 23:28:48 +0600

    Add files via upload

---
+++ Namta.java
@@ -0,0 +1,15 @@
+package com.extra;
+
+import java.util.Scanner;
+
+public class Namta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int t = scanner.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(t+"*"+i+"="+t*i);
+        }
+    }
+    
+}
